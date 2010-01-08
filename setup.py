dfc107e223b8ef85a6e155b0862aa047483405ed
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-01-08 12:09:30 -0600

    adding README, INSTALL, LICENSE files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.4.2'
+version = '0.4.3'
 
 LONG = """
 Cement is a CLI Application Framework for Python. It promotes code re-use by
