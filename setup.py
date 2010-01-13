512595d65285a45851c75b8d0058e16b986bc405
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-01-13 17:56:44 -0600

    Upping to 0.4.5, development version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.4.4'
+version = '0.4.5'
 
 LONG = """
 Cement is a CLI Application Framework for Python. It promotes code re-use by
