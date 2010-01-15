15da6474a11cebd03f65955116ce17e3c6e59184
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-01-15 17:30:51 -0600

    Fixing pylint errors

--- cement/core/app_setup.py
+++ cement/core/app_setup.py
@@ -1,22 +1,19 @@
 """Cement methods to setup the framework for applications using it."""
-import sys, os
-import re
-from pkg_resources import get_distribution
-from optparse import OptionParser, IndentedHelpFormatter
 
-from cement import plugins as cement_plugins
-from cement import hooks, namespaces
-from cement.core.exc import *
-from cement.core.configuration import *
+from pkg_resources import get_distribution
+
+from cement import namespaces
+from cement.core.configuration import CEMENT_API, set_config_opts_per_file
+from cement.core.configuration import validate_config, get_default_config
 from cement.core.plugin import load_all_plugins
 from cement.core.namespace import CementNamespace, define_namespace
 from cement.core.log import setup_logging, get_logger
-from cement.core.opt import init_parser, get_options, Options
 from cement.core.hook import define_hook, run_hooks
 
 log = get_logger(__name__)    
+
 def register_default_hooks():
-    # define default hooks
+    """Register Cement framework hooks."""
     define_hook('options_hook')
     define_hook('post_options_hook')
     define_hook('validate_config_hook')
@@ -30,8 +27,6 @@ def lay_cement(default_app_config=None, version_banner=None):
     config => dict containing application config.
     version_banner => Option txt displayed for --version
     """    
-    global namespaces, log
-    
     vb = version_banner    
     if not version_banner:
         vb = """%s version %s""" % (
@@ -62,7 +57,8 @@ def lay_cement(default_app_config=None, version_banner=None):
     load_all_plugins()
     
     # allow plugins to add config validation
-    for res in run_hooks('validate_config_hook', config=namespaces['global'].config):
+    for res in run_hooks('validate_config_hook', 
+                         config=namespaces['global'].config):
         pass
     
 
