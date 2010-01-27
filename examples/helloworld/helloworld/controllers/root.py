430efdeabbdced9608540e61e4b501abbbea24f3
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-01-27 11:44:43 -0600

    Adding root template dir

--- examples/helloworld/helloworld/controllers/root.py
+++ examples/helloworld/helloworld/controllers/root.py
@@ -1,12 +1,12 @@
 """
 This is the RootController for the helloworld application.  Usually this
 file will remain pretty sparse, however it is a good place for miscellaneous
-commands that might night need there own namespace or controller.  
+commands that might not need there own namespace or controller.  
 """
 
 from cement.core.controller import CementController, expose
 
 class RootController(CementController):
-    @expose('helloworld.templates.error', is_hidden=True)
+    @expose('helloworld.templates.root.error', is_hidden=True)
     def error(self, errors={}):
         return dict(errors=errors)
