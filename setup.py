a72633106fb2cf832b5754f61604cf5f20d44e6c
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-02-05 21:03:41 -0600

    0.6 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.5.4'
+version = '0.6'
 
 LONG = """
 Cement is an advanced CLI Application Framework for Python. It promotes code 
