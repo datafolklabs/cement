ab9a565a5c3fcf699ebb76b81f4c81a76894724e
BJ Dierkes <derks@netbook.derks>
2010-02-19 16:40:26 -0600

    adding changes to changelog

--- examples/helloworld/setup.py
+++ examples/helloworld/setup.py
@@ -16,7 +16,7 @@ setup(name='helloworld',
     install_requires=[
         "ConfigObj",
         "Genshi",
-        "Cement >=0.6, <0.7",
+        "Cement >=0.6.1, <0.7",
         # Uncomment to use shared plugins from The Rosendale Project.
         #"Rosendale",
         ],
