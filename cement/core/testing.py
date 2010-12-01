261302ab9eb159e26b02acf6756b4153815b38de
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-12-01 04:34:40 -0600

    removing commented code

--- cement/core/testing.py
+++ cement/core/testing.py
@@ -38,35 +38,6 @@ def simulate(args=[]):
         raise CementRuntimeError, "args must be set properly."
         
     sys.argv = args
-    
-    #namespace = 'root' # default
-    #controller_namespace = 'root' # default
-    #    
-    #if sys.argv[1] in namespaces:
-    #    namespace = sys.argv[1]
-    #    
-    #for nam in namespaces:
-    #    if sys.argv[1] in namespaces[nam].commands:
-    #        controller_namespace = namespaces[nam].commands[sys.argv[1]]['controller_namespace']
-    #        break 
-    
-    # get the command to run
-    #if namespace == 'root':
-    #    cmd = sys.argv[1]
-    #else:
-    #    if not len(sys.argv) >= 3:
-    #        raise CementRuntimeError, "A subcommand (additional arg) is required."
-    #    else:
-    #        cmd = sys.argv[2]
-
-    # unconvert dashes
-    #cmd = re.sub('-', '_', cmd)
-    #
-    #print controller_namespace
-    #print namespace
-    #print cmd
-    
-    #(opts, args) = parse_options(controller_namespace, ignore_conflicts=True)
-    #(res_dict, output_txt) = run_controller_command(controller_namespace, cmd, opts, args) 
-    (res_dict, output_txt) = run_command(cmd_name=sys.argv[1], ignore_conflicts=True)
+    cmd = re.sub('-', '_', sys.argv[1])
+    (res_dict, output_txt) = run_command(cmd_name=cmd, ignore_conflicts=True)
     return (res_dict, output_txt)
