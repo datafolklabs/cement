6a783032bf6ac61392ac53ab01edb7db636e2dae
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-12-06 20:13:29 -0600

    touch up readme's a bit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '0.8.13'
 
 LONG = """
 Cement is an advanced CLI Application Framework for Python. It promotes code
-re-use by way of a plugin system that enabled plugins to be shared between any 
+re-use by way of a plugin system that enables plugins to be shared between any 
 application built on Cement.  The MVC and overall framework design is very 
 much inspired by the TurboGears2 web framework.  Its goal is to introduce a 
 standard, and feature-full platform for both simple and complex command line
