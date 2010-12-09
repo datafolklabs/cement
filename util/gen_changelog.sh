229aaa7a33d657296f94549387297f651901f143
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-12-08 21:51:08 -0600

    changing up a few things

--- util/gen_changelog.sh
+++ util/gen_changelog.sh
@@ -2,27 +2,17 @@
 
 previous=$(git describe | awk -F '-' {' print $1 '})
 version=$(python -c "from pkg_resources import get_distribution ; print get_distribution('cement.core').version")
-dir=~/cement-${version}
 date=$(date)
 tmpfile=$(mktemp /tmp/cementXXXXX)
 status=$(git status --porcelain)
-
-if [ -e "${version}" ]; then
-    echo "release dir already exists $dir"
-fi
 
 if [ "${status}" != "" ]; then
     echo "must commit all changes first"
     exit 1
 fi
 
-mkdir ${dir}/doc
-mkdir ${dir}/download
-mkdir ${dir}/pypi
-
-git mv ChangeLog ChangeLog.orig
 changes=$(git --no-pager log --pretty=" -  %s [%h]" ${previous}..)
-cat >${tmpfile} <<EOF
+cat <<EOF
 #
 # Tracker Issues and Commits referenced here can be viewed online as:
 #
@@ -37,7 +27,3 @@ ${changes}
 
 EOF
 
-vim ${tmpfile}
-echo ${tmpfile}
-#mv ${tmpfile} ChangeLog
-#cat ChangeLog.orig | grep -v ^# >> ChangeLog
