0c845d8cfb1472f63d84cb79abe50f3d2b2474ec
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-12-08 22:08:52 -0600

    remove comments

--- util/gen_changelog.sh
+++ util/gen_changelog.sh
@@ -7,18 +7,12 @@ tmpfile=$(mktemp /tmp/cementXXXXX)
 status=$(git status --porcelain)
 
 if [ "${status}" != "" ]; then
-    echo "must commit all changes first"
-    exit 1
+    echo
+    echo "WARNING: not all changes committed"
 fi
 
 changes=$(git --no-pager log --pretty=" -  %s [%h]" ${previous}..)
 cat <<EOF
-#
-# Tracker Issues and Commits referenced here can be viewed online as:
-#
-#   Commit: https://github.com/derks/cement/commit/XXXXXXX
-#    Issue: https://github.com/derks/cement/issues/#issue/XX
-#
 
 ${version} - $date
 ------------------------------------------------------------------------------
