b027d3abdce2198ac7d585e1903c652402d4827e
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-12-09 20:16:44 -0600

    bumping version to 0.8.15 (dev)

--- src/cement/setup.py
+++ src/cement/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-VERSION = '0.8.14'
+VERSION = '0.8.15'
 
 LONG = """
 Cement is an advanced CLI Application Framework for Python. This package 
