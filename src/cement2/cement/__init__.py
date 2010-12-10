625c0f8dda0d42d4e56d59d71118da31a0dbec3f
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-12-10 07:33:19 -0600

    moving config to backend module

--- src/cement2/cement/__init__.py
+++ src/cement2/cement/__init__.py
@@ -1,16 +1,5 @@
 
-# global config dictionary
-config = {}
-config['app_name'] = None
-config['app_module'] = None
-config['app_egg'] = None
-config['debug'] = False
-config['log_file'] = None
-config['log_level'] = 'INFO'
-config['log_to_console'] = True
-config['log_max_bytes'] = None
-config['log_max_files'] = 4
-config['log_file_formatter'] = None
-config['log_console_formatter'] = None
+from cement.core.backend import init_config
+from cement.core.app import CementApp
 
 
