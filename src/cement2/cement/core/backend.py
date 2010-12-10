4873e3b1144c351183f15f97f22c02da9c436ff4
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-12-10 07:32:18 -0600

    adding backend module

--- src/cement2/cement/core/backend.py
+++ src/cement2/cement/core/backend.py
@@ -1,6 +1,25 @@
 
 import sys
 
+# global config dictionary
+config = {}
+config['app_name'] = None
+config['app_module'] = None
+config['app_egg'] = None
+config['debug'] = False
+config['log_file'] = None
+config['log_level'] = 'INFO'
+config['log_to_console'] = True
+config['log_max_bytes'] = None
+config['log_max_files'] = 4
+config['log_file_formatter'] = None
+config['log_console_formatter'] = None
+config['log_handler'] = 'logging'
+
+def init_config():
+    _config = config.copy()
+    return _config
+    
 # global handlers dict
 handlers = {}
 
