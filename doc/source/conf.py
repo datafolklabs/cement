80c8b0df7529c43c37b253ffe36bf544df317393
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2010-12-09 19:11:33 -0600

    add new paths for sys.path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,8 @@ RELEASE = '0.8.14'
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('../../src/cement/'))
+sys.path.append(os.path.abspath('../../src/cement.devtools/'))
 
 # -- General configuration -----------------------------------------------------
 
