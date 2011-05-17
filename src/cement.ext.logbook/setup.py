8c2ed7d4b0ba9d769f7e9229f47e42f1873b1732
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2011-05-17 06:34:11 -0500

    moving logbook to own ext

---
+++ src/cement.ext.logbook/setup.py
@@ -0,0 +1,47 @@
+
+from setuptools import setup, find_packages
+import sys, os
+
+VERSION = '1.9.1'
+
+LONG = """
+Cement is an advanced CLI Application Framework for Python. This package 
+provides the logbook framework extension allowing applications to use
+the logbook log handler.
+
+The Cement CLI Application Framework is Open Source and is distributed under 
+The MIT License.  
+
+
+MORE INFORMATION:
+
+All documentation is available from the official website:
+
+    http://builtoncement.org
+    
+"""
+
+setup(name='cement.ext.logbook',
+    version=VERSION,
+    description="LogBook Framework Extension for Cement",
+    long_description=LONG,
+    classifiers=[], 
+    keywords='cli framework cement',
+    author='BJ Dierkes',
+    author_email='wdierkes@5dollarwhitebox.org',
+    url='http://builtoncement.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "logbook", 
+        ],
+    setup_requires=[
+        ],
+    entry_points="""
+    """,
+    namespace_packages=[
+        'cement.ext'
+        ],
+    )
