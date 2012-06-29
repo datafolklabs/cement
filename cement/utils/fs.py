471a8e29fdcd3b8bf2d0a9b8ae88845cfcb720f7
BJ Dierkes <wdierkes@rackspace.com>
2012-06-29 02:33:19 -0500

    more re-organization

--- cement/utils/fs.py
+++ cement/utils/fs.py
@@ -3,4 +3,14 @@
 import os
 
 def abspath(path):
+    """
+    Return an absolute path, while also expanding the '~' user directory
+    shortcut.
+    
+    Required Arguments:
+    
+        path
+            The original path to expand.
+            
+    """
     return os.path.abspath(os.path.expanduser(path))
