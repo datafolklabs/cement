e512b54e9286b6c05b15191bfeb33a66bcb640a8
BJ Dierkes <wdierkes@rackspace.com>
2012-07-05 15:12:59 -0500

    bumping version to 1.9.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-VERSION = '1.9.12'
+VERSION = '1.9.13'
 
 LONG = """
 Cement is an advanced CLI Application Framework for Python.  Its goal is to 
