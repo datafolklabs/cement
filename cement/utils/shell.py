a3b73a5f0ae51ca28cc845c4950957b0c3e28aaf
BJ Dierkes <wdierkes@rackspace.com>
2012-07-30 21:55:16 -0500

    Resolves Issue #138

--- cement/utils/shell.py
+++ cement/utils/shell.py
@@ -2,7 +2,7 @@
 
 from subprocess import Popen, PIPE
             
-def exec_cmd(cmd_args):
+def exec_cmd(cmd_args, shell=False):
     """
     Execute a shell call using Subprocess.
     
@@ -11,15 +11,19 @@ def exec_cmd(cmd_args):
         cmd_args
             List of command line arguments.
             
+        shell
+            Boolean option.  See `Subprocess <http://docs.python.org/library/subprocess.html>`_
+            Default: False
+            
     Return: tuple (stdout, stderr, return_code)
     
     """
-    proc = Popen(cmd_args, stdout=PIPE, stderr=PIPE)
+    proc = Popen(cmd_args, stdout=PIPE, stderr=PIPE, shell=shell)
     (stdout, stderr) = proc.communicate()
     proc.wait()
     return (stdout, stderr, proc.returncode)
     
-def exec_cmd2(cmd_args):
+def exec_cmd2(cmd_args, shell=False):
     """
     Similar to exec_cmd, however does not capture stdout, stderr (therefore
     allowing it to print to console).
@@ -29,9 +33,13 @@ def exec_cmd2(cmd_args):
         cmd_args
             List of command line arguments.
             
+        shell
+            Boolean option.  See `Subprocess <http://docs.python.org/library/subprocess.html>`_
+            Default: False
+            
     Return: int (return_code)
     
     """
-    proc = Popen(cmd_args)
+    proc = Popen(cmd_args, shell=shell)
     proc.wait()
     return proc.returncode
--- tests/utils/shell_tests.py
+++ tests/utils/shell_tests.py
@@ -8,7 +8,23 @@ class ShellUtilsTestCase(test.CementTestCase):
         self.eq(ret, 0)
         self.eq(out, b'KAPLA!\n')
         
+    def test_exec_cmd_shell_true(self):
+        out, err, ret = shell.exec_cmd(['echo KAPLA!'], shell=True)
+        self.eq(ret, 0)
+        self.eq(out, b'KAPLA!\n')
+        
     def test_exec_cmd2(self):
         ret = shell.exec_cmd2(['echo'])
         self.eq(ret, 0)
         
+    def test_exec_cmd2_shell_true(self):
+        ret = shell.exec_cmd2(['echo johnny'], shell=True)
+        self.eq(ret, 0)
+    
+    def test_exec_cmd_bad_command(self):
+        out, err, ret = shell.exec_cmd(['false'])
+        self.eq(ret, 1)
+    
+    def test_exec_cmd2_bad_command(self):
+        ret = shell.exec_cmd2(['false'])
+        self.eq(ret, 1)
