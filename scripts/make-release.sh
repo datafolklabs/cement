dccd46e67e152b3e22e07b018e58c317e421504a
BJ Dierkes <wdierkes@rackspace.com>
2012-09-07 15:29:34 -0500

    Cleaned up output, added PEP8 checks

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -9,33 +9,67 @@ version=$1
 
 res=$(git tag | grep $version)
 if [ $? != 0 ]; then
-    echo "Git tag ${version} does not exist."
-    exit
+    echo -n "Verifying Git Tag: "
+    echo "FAIL"
+    exit 1
 fi
 
 short=$(echo $version | awk -F . {' print $1"."$2 '})
 dir=~/cement-${version}
 tmpdir=$(mktemp -d -t cement-$version)
 
-mkdir ${dir}
-mkdir ${dir}/doc
-mkdir ${dir}/source
+echo
+echo "Generating ${dir}"
+echo '-----------------------------------------------------------------------'
 
-# run tests
-python setup.py nosetests
-if [ $? != 0 ]; then
-    echo "Tests fail!"
+echo -n "Creating Destination Directory: "
+if [ -e ${dir} ]; then
+    echo "FAIL (already exists)"
+    exit 1
+else
+    mkdir -p ${dir}
+    mkdir -p ${dir}/doc
+    mkdir -p ${dir}/source
+    echo "OK"
+fi
+
+echo -n "Running Nose Tests: "
+python setup.py nosetests --verbosity 0 2>/dev/null 1>/dev/null
+if [ $? == 0 ]; then
+    echo 'OK'
+else
+    echo "FAIL"
     exit 1
 fi
 
+echo -n "PEP8 Compliant: "
+pep8 cement/ 2>/dev/null 1>/dev/null
+if [ $? == 0 ]; then
+    echo 'OK'
+else
+    echo 'FAIL'
+    exit 1
+fi
+    
+
 # all
+echo -n "Generating Release Files: "
 git archive ${version} --prefix=cement-${version}/ | gzip > ${dir}/source/cement-${version}.tar.gz
 cp -a ${dir}/source/cement-${version}.tar.gz $tmpdir/
+echo 'OK'
 
-pushd $tmpdir
-    tar -zxvf cement-${version}.tar.gz
-    pushd cement-${version}/
-        sphinx-build doc/source ${dir}/doc
-    popd
-popd
+pushd $tmpdir >/dev/null
+    tar -zxf cement-${version}.tar.gz
+    pushd cement-${version}/ >/dev/null
+        echo -n "Building Documentation: "
+        sphinx-build -W doc/source ${dir}/doc 2>/dev/null 1>/dev/null
+        if [ $? == 0 ]; then
+            echo 'OK'
+        else
+            echo "FAIL"
+            exit 1
+        fi
+    popd >/dev/null
+popd >/dev/null
 
+echo
