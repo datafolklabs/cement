5ba32dccca1f7cff0510af0999338a77a999e8be
BJ Dierkes <wdierkes@rackspace.com>
2012-11-01 17:23:56 -0500

    Removimg 3.3 from travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,5 +2,6 @@
 
 pip install argparse coverage nose --use-mirrors
 pip install -r requirements.txt --use-mirrors
+pip install -r requirements-dev.txt --use-mirrors
 python setup.py nosetests
 exit $?
