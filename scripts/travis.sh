cf8e9fbb4499f5988fdb41267933f48f8d795a34
BJ Dierkes <derks@bjdierkes.com>
2013-03-14 01:25:10 -0500

    Only install argparse if Py < 2.7

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#pip install argparse coverage nose --use-mirrors
+PYCHECK=$(python -c 'import sys; print sys.version_info < (2, 7)')
+if [ "$PYCHECK" == "True" ]; then
+    pip install argparse --use-mirrors
+fi
+
 pip install coverage nose --use-mirrors
 pip install -r requirements.txt --use-mirrors
 pip install -r requirements-dev.txt --use-mirrors
