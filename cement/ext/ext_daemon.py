a07ebba03c1708f2b516aa5cb962c584f0b0e275
BJ Dierkes <derks@datafolklabs.com>
2013-10-24 18:07:58 -0500

    Adding docs for daemon extension

--- cement/ext/ext_daemon.py
+++ cement/ext/ext_daemon.py
@@ -1,11 +1,167 @@
-"""Daemon Framework Extension"""
+"""
+The Daemon Framework Extension enables applications built on Cement to easily
+perform standard 'daemon' functions.
+
+Requirements
+------------
+
+ * Python 2.6+, Python 3+
+
+Features
+--------
+
+ * Configurable runtime user and group
+ * Adds the --daemon command line option
+ * Adds app.daemonize() function to trigger daemon functionality where
+   necessary (either in a cement pre_run hook or an application controller
+   sub-command, etc).
+ * Manages a pid file including cleanup on app.close()
+
+Configuration
+-------------
+
+The daemon extension is configurable with the following settings under the
+[daemon] section.
+
+    * **user** - The user name to run the process as.
+      Default: os.environ['USER']
+    * **group** - The group name to run the process as.
+      Default: The primary group of the 'user'.
+    * **dir** - The directory to run the process in.
+      Default: /
+    * **pid_file** - The filesystem path to store the PID (Process ID) file.
+      Default: None
+    * **umask** - The umask value to pass to os.umask().
+      Default: 0
+
+
+
+Configurations can be passed as defaults to a CementApp:
+
+.. code-block:: python
+
+    from cement.core import foundation
+    from cement.utils.misc import init_defaults
+
+    defaults = init_defaults('myapp', 'daemon')
+    defaults['daemon']['user'] = 'myuser'
+    defaults['daemon']['group'] = 'mygroup'
+    defaults['daemon']['dir'] = '/var/lib/myapp/'
+    defaults['daemon']['pid_file'] = '/var/run/myapp/myapp.pid'
+    defaults['daemon']['umask'] = 0
+
+    app = foundation.CementApp('myapp', config_defaults=defaults)
+
+
+
+Application defaults are then overridden by configurations parsed via a
+[daemon] config section in any of the applications configuration paths.  An
+example configuration block would look like:
+
+.. code-block:: text
+
+    [daemon]
+    user = myuser
+    group = mygroup
+    dir = /var/lib/myapp/
+    pid_file = /var/run/myapp/myapp.pid
+    umask = 0
+
+
+Usage
+-----
+
+The following example shows how to add the daemon extension, as well as
+trigger daemon functionality before app.run() is called.
+
+.. code-block:: python
+
+    from time import sleep
+    from cement.core import foundation
+
+    app = foundation.CementApp('myapp', extensions=['daemon'])
+
+    try:
+        app.setup()
+        app.daemonize()
+        app.run()
+
+        count = 0
+        while True:
+            count = count + 1
+            print('Iteration: %s' % count)
+            sleep(10)
+    finally:
+        app.close()
+
+
+An alternative to the above is to put app.daemonize() within a framework hook:
+
+.. code-block:: python
+
+    from cement.core import hook
+
+    def make_daemon(app):
+        app.daemonize()
+
+    hook.register('pre_run', make_daemon)
+
+
+Finally, some applications may prefer to only daemonize certain sub-commands
+rather than the entire parent application.  For example:
+
+.. code-block:: python
+
+    from cement.core import foundation, controller, handler
+
+    class MyAppBaseController(controller.CementBaseController):
+        class Meta:
+            label = 'base'
+
+        @controller.expose(help="run the daemon command.")
+        def run_forever(self):
+            from time import sleep
+            self.app.daemonize()
+
+            count = 0
+            while True:
+                count = count + 1
+                print(count)
+                sleep(10)
+
+    app = foundation.CementApp('myapp',
+        extensions=['daemon'],
+        base_controller=MyAppBaseController,
+        )
+
+    try:
+        app.setup()
+        app.run()
+    finally:
+        app.close()
+
+
+By default, even after app.daemonize() is called... the application will
+continue to run in the foreground, but will still manage the pid and
+user/group switching.  To detach a process and send it to the background you
+simply pass the '--daemon' option at command line.
+
+.. code-block:: text
+
+    $ python example.py --daemon
+
+    $ ps -x | grep example
+    37421 ??         0:00.01 python example2.py --daemon
+    37452 ttys000    0:00.00 grep example
+
+"""
 
 import os
 import sys
 import io
 import pwd
 import grp
-from ..core import handler, hook, backend, exc
+from ..core import handler, hook, exc
 from ..utils.misc import minimal_logger
 
 LOG = minimal_logger(__name__)
