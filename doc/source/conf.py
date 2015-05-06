c2acc68bf996c6682cec15dd7b0abed237f3bb24
BJ Dierkes <derks@datafolklabs.com>
2015-05-06 00:55:12 -0500

    Bump Year in Copyrights

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -86,7 +86,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Cement'
-copyright = u'2009-2014, Data Folk Labs, LLC'
+copyright = u'2009-2015, Data Folk Labs, LLC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
