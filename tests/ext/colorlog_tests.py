9873a8c6866e67d1a7bfc438206f91a3c0bc0a7f
BJ Dierkes <derks@datafolklabs.com>
2015-06-12 14:12:32 -0500

    Resolves Issue #314

--- tests/ext/colorlog_tests.py
+++ tests/ext/colorlog_tests.py
@@ -1,19 +1,48 @@
 """Tests for cement.ext.ext_colorlog."""
 
-from cement.ext import ext_colorlog
+import os
+from cement.ext.ext_colorlog import ColorLogHandler
 from cement.utils import test
-from cement.utils.misc import rando
+from cement.utils.misc import rando, init_defaults
 
 APP = rando()[:12]
 
+
 class ColorLogExtTestCase(test.CementExtTestCase):
+
     def setUp(self):
-        self.app = self.make_app(APP, 
-                        extensions=['colorlog'], 
-                        log_handler='colorlog',
-                        )
+        super(ColorLogExtTestCase, self).setUp()
+        log_file = os.path.join(self.tmp_dir, '%s.log' % APP)
+        defaults = init_defaults()
+        defaults['log.colorlog'] = dict(
+            file=log_file,
+            level='DEBUG',
+        )
+        self.app = self.make_app(APP,
+                                 config_defaults=defaults,
+                                 extensions=['colorlog'],
+                                 log_handler='colorlog',
+                                 )
 
     def test_colorlog(self):
         self.app.setup()
         self.app.run()
+        self.app.log.info('this is an info message')
         self.app.close()
+
+    def test_colorized_log_files(self):
+        log_file = os.path.join(self.tmp_dir, '%s.log' % APP)
+        defaults = init_defaults()
+        defaults['log.colorlog'] = dict(
+            file=log_file,
+            level='DEBUG',
+        )
+        self.app = self.make_app(APP,
+                                 config_defaults=defaults,
+                                 extensions=['colorlog'],
+                                 log_handler=ColorLogHandler(
+                                     colorize_log_files=True),
+                                 )
+        self.app.setup()
+        self.app.run()
+        self.app.log.info('this is an info message')
