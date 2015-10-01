1fa7485e48c7953b0276c0bb7c766f7e6854936e
BJ Dierkes <derks@datafolklabs.com>
2015-10-01 17:08:50 -0500

    Drop --use-mirrors For Travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -5,14 +5,14 @@ sed -i 's/with-coverage=1/with-coverage=0/g' setup.cfg
 
 PYCHECK=$(python -c 'import sys; print(sys.version_info < (2, 7))')
 if [ "$PYCHECK" == "True" ]; then
-    pip install argparse --use-mirrors
+    pip install argparse
 fi
 
 PYCHECK=$(python -c 'import sys; print(sys.version_info > (3, 0))')
 if [ "$PYCHECK" == "True" ]; then
-    pip install -r requirements-dev-py3-linux.txt --use-mirrors
+    pip install -r requirements-dev-py3-linux.txt
 else
-    pip install -r requirements-dev-linux.txt --use-mirrors
+    pip install -r requirements-dev-linux.txt
 fi
 
 python setup.py nosetests
