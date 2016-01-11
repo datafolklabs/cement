bf5ad9edcd58b3d4e3f559342be2b2465187e925
BJ Dierkes <derks@datafolklabs.com>
2016-01-11 11:27:34 -0600

    Resolve ArgComplete Tests

--- tests/ext/argcomplete_tests.py
+++ tests/ext/argcomplete_tests.py
@@ -2,17 +2,31 @@
 
 import os
 from cement.ext import ext_argcomplete
+from cement.ext.ext_argparse import ArgparseController, expose
 from cement.utils import test
 from cement.utils.misc import rando
 
 APP = rando()[:12]
 
+class MyBaseController(ArgparseController):
+    class Meta:
+        label = 'base'
+
+    @expose()
+    def default(self):
+        pass
 
 class ArgcompleteExtTestCase(test.CementExtTestCase):
 
     def setUp(self):
         super(ArgcompleteExtTestCase, self).setUp()
-        self.app = self.make_app(APP, extensions=['argcomplete'])
+        self.app = self.make_app(APP, 
+            base_controller=MyBaseController,
+            extensions=[
+                'argparse',
+                'argcomplete'
+                ],
+            )
 
     def test_argcomplete(self):
         # not really sure how to test this for reals... but let's atleast get
