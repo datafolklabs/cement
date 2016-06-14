b1cb1d45d33909ddd97572b7416a4cc85fcb4b04
BJ Dierkes <derks@datafolklabs.com>
2016-06-14 17:22:08 -0500

    Work on Vagrant (Maybe Broken?)

--- scripts/vagrant/up.sh
+++ scripts/vagrant/up.sh
@@ -1,45 +1,66 @@
 #!/bin/bash
+set -e
 
 cd /vagrant
 
-sudo apt-get update && \
-sudo apt-get upgrade -y && \
-sudo apt-get dist-upgrade -y && \
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo apt-key adv \
+    --keyserver hkp://p80.pool.sks-keyservers.net:80 \
+    --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo add-apt-repository \
+    -y "deb https://apt.dockerproject.org/repo ubuntu-trusty main"
+sudo apt-get update
+sudo apt-cache policy docker-engine
+
 sudo apt-get install -y \
     python \
-    python-dev \
     python-pip \
+    python-dev \
+    python-virtualenv \
     python3 \
     python3-dev \
     python3-pip \
     memcached \
     libmemcached-dev \
     zlib1g-dev \
-    docker.io
+    docker-engine
 
 sudo apt-get autoremove -y 
+sudo pip3 install virtualenv
 
 # for docker stuff
 sudo usermod -aG docker vagrant
-sudo pip install -U fig
 
-PY3_VER=$(python3 -c 'import sys; print("%s.%s" % (sys.version_info[0], sys.version_info[1]))')
-sudo pip install virtualenv
-virtualenv ~/.env/cement
-sudo pip3 install virtualenv
-virtualenv-${PY3_VER} ~/.env/cement-py3
+### fix me - install docker-compose here
+
+python /usr/bin/virtualenv /vagrant/.env/cement-py2
+python3 /usr/bin/virtualenv /vagrant/.env/cement
 
 # for tests
-killall memcached
-memcached &
+sudo /etc/init.d/memcached stop
+sudo /etc/init.d/memcached start
 
 deactivate ||:
 
-source ~/.env/cement/bin/activate
+source /vagrant/.env/cement-py2/bin/activate 
 pip install -r requirements-dev-linux.txt
 python setup.py develop
+deactivate
 
-source ~/.env/cement-py3/bin/activate
+source /vagrant/.env/cement/bin/activate
 pip install -r requirements-dev-py3-linux.txt
 python setup.py develop
+deactivate
+
+cat >>~/.bashrc <<EOF
+export PS1="\[\e[0;33m\]\u@\h \[\e[1;35m\]\W\[\e[0m\] \[\e[0m\]\$ "
+cd /vagrant
+EOF
+
+exit 0
 
