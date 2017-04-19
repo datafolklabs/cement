c67fff041110d36f41481fd15e58834f12546370
BJ Dierkes <derks@datafolklabs.com>
2017-04-18 22:54:58 -0500

    Update Travis CI Script

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
+set -e
 
 rm -f .coverage
-sed -i 's/with-coverage=1/with-coverage=0/g' setup.cfg
-
-PYCHECK=$(python -c 'import sys; print(sys.version_info < (2, 7))')
-if [ "$PYCHECK" == "True" ]; then
-    pip install argparse
-fi
-
-PYCHECK=$(python -c 'import sys; print(sys.version_info > (3, 0))')
-if [ "$PYCHECK" == "True" ]; then
-    REQ_BASE='requirements-dev-py3'
-else
-    REQ_BASE='requirements-dev'
-fi
-
-if [ "$(uname)" == 'Linux' ]; then
-    pip install -r $REQ_BASE-linux.txt
-else
-    pip install -r $REQ_BASE.txt
-fi
-
-python setup.py nosetests
-exit $?
-
+pip install -r requirements-dev.txt
+make test
