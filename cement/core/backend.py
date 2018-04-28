0b6e2a544c904b81de842eb22a0503e283268490
BJ Dierkes <derks@datafolklabs.com>
2018-04-28 16:42:17 -0500

    Add Required Deps for PyPi Markdown

--- cement/core/backend.py
+++ cement/core/backend.py
@@ -1,6 +1,6 @@
 """Cement core backend module."""
 
-VERSION = (2, 10, 10, 'final', 0)  # pragma: nocover
+VERSION = (2, 10, 12, 'final', 0)  # pragma: nocover
 
 # global hooks/handlers (DEPRECATED)
 __handlers__ = {}   # pragma: nocover
