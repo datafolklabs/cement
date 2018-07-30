f298bad63dcb5cdbf196314c8d6ab74278bd20f1
BJ Dierkes <derks@datafolklabs.com>
2018-07-29 19:01:31 -0500

    Removed Commented Lines

--- tests/core/test_output.py
+++ tests/core/test_output.py
@@ -1,9 +1,4 @@
 
-# import os
-# from pytest import raises
-#
-# from cement.core.foundation import TestApp
-# from cement.core.exc import FrameworkError
 from cement.core.output import OutputInterface, OutputHandler
 
 
@@ -28,51 +23,6 @@ class TestOutputHandler(object):
         assert h._meta.label == 'my_output_handler'
 
 
-# class TestTemplateOutputHandler(object):
-#     def test_subclassing(self):
-#         class MyTemplateOutputHandler(TemplateOutputHandler):
-#             class Meta:
-#                 label = 'my_template_output_handler'
-#
-#             def render(self, *args, **kw):
-#                 pass
-#
-#         h = MyTemplateOutputHandler()
-#         assert h._meta.interface == 'output'
-#         assert h._meta.label == 'my_template_output_handler'
-
-
 # app functionality and coverage tests
 
 TEST_TEMPLATE = "%(foo)s"
-
-
-# class MyOutputHandler(TemplateOutputHandler):
-#
-#     class Meta:
-#         label = 'my_output_handler'
-#
-#     def render(self, data, template):
-#         content = self.load_template(template)
-#         return content % data
-#
-#
-# def test_load_template_from_file(tmp):
-#     template = os.path.join(tmp.dir, 'mytemplate.txt')
-#
-#     f = open(template, 'w')
-#     f.write(TEST_TEMPLATE)
-#     f.close()
-#
-#     class MyApp(TestApp):
-#         class Meta:
-#             template_dir = tmp.dir
-#             output_handler = MyOutputHandler
-#
-#     with MyApp() as app:
-#         app.run()
-#         assert app.render({'foo': 'bar'}, 'mytemplate.txt') == 'bar'
-#
-#         # try and render a missing template
-#         with raises(FrameworkError, match='Could not locate template: .*'):
-#             app.render({'foo': 'bar'}, 'missing-template.txt')
