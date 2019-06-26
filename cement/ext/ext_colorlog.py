fba712623b2e36abf7196d21b0f0c53432a09c58
Spyros Vlachos <devspyrosv@users.noreply.github.com>
2019-06-26 18:56:54 +0300

    Trailing whitespace (W291)
    :-)

--- cement/ext/ext_colorlog.py
+++ cement/ext/ext_colorlog.py
@@ -64,7 +64,7 @@ class ColorLogHandler(LoggingLogHandler):
 
     def _get_console_format(self):
         format = super(ColorLogHandler, self)._get_console_format()
-        colorize = self.app.config.get('log.{}'.format(self.Meta.label), 
+        colorize = self.app.config.get('log.{}'.format(self.Meta.label),
                                        'colorize_console_log')
         if sys.stdout.isatty() or 'CEMENT_TEST' in os.environ:
             if is_true(colorize):
@@ -73,14 +73,14 @@ class ColorLogHandler(LoggingLogHandler):
 
     def _get_file_format(self):
         format = super(ColorLogHandler, self)._get_file_format()
-        colorize = self.app.config.get('log.{}'.format(self.Meta.label), 
+        colorize = self.app.config.get('log.{}'.format(self.Meta.label),
                                        'colorize_file_log')
         if is_true(colorize):
             format = "%(log_color)s" + format
         return format
 
     def _get_console_formatter(self, format):
-        colorize = self.app.config.get('log.{}'.format(self.Meta.label), 
+        colorize = self.app.config.get('log.{}'.format(self.Meta.label),
                                        'colorize_console_log')
         if sys.stdout.isatty() or 'CEMENT_TEST' in os.environ:
             if is_true(colorize):
@@ -99,7 +99,7 @@ class ColorLogHandler(LoggingLogHandler):
         return formatter
 
     def _get_file_formatter(self, format):
-        colorize = self.app.config.get('log.{}'.format(self.Meta.label), 
+        colorize = self.app.config.get('log.{}'.format(self.Meta.label),
                                        'colorize_file_log')
         if is_true(colorize):
             formatter = self._meta.formatter_class(
