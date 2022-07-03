2a5edfaebb6a2b67e8886c1310d84ed0a110460d
BJ Dierkes <derks@datafolklabs.com>
2022-07-02 23:13:30 -0500

    Fix Python 3.7 on Travis CI

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# fix for Python 3.7 on Travis
+# https://travis-ci.community/t/build-error-for-python-3-7-on-two-different-projects/12895/3
+pip install -U importlib_metadata
+
 rm -f .coverage
 pip install -r requirements-dev.txt
 make test
