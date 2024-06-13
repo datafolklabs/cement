92cf147a6479bbbf9639f59c4a8915e24ca5873d
sigma67 <sigma67.github@gmail.com>
2024-06-13 20:41:26 +0200

    remove cli/contrib

--- cement/cli/main.py
+++ cement/cli/main.py
@@ -1,7 +1,6 @@
 
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), 'contrib'))
 
 from cement import App, CaughtSignal  # noqa: E402
 from .controllers.base import Base    # noqa: E402
