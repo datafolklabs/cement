f5575d88962c328fbd8beab5a03d47ea08548790
sigma67 <sigma67.github@gmail.com>
2024-06-13 21:23:47 +0200

    add travis 3.13 workaround

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -e
 
-# removed - 3.7 is EOL: https://github.com/datafolklabs/cement/issues/658
-# fix for Python 3.7 on Travis
-# https://travis-ci.community/t/build-error-for-python-3-7-on-two-different-projects/12895/3
-# pip install -U importlib_metadata
-
 docker-compose up -d mailpit 2>&1 >/dev/null
 
 sleep 10
 
 rm -f .coverage
-pdm venv create
+pdm venv create $PYTHON_VERSION
 pdm install
 make test
