2bc559a30d1b8034bf98075b224328650adebcf5
sigma67 <sigma67.github@gmail.com>
2025-05-06 10:07:03 +0200

    fix __all__

--- cement/__init__.py
+++ cement/__init__.py
@@ -10,3 +10,21 @@ from .ext.ext_argparse import expose as ex
 from .utils.misc import init_defaults, minimal_logger
 from .utils import misc, fs, shell
 from .utils.version import get_version
+
+__all__ = [
+    "App",
+    "TestApp",
+    "Interface",
+    "Handler",
+    "FrameworkError",
+    "InterfaceError",
+    "CaughtSignal",
+    "Controller",
+    "ex",
+    "init_defaults",
+    "minimal_logger",
+    "misc",
+    "fs",
+    "shell",
+    "get_version",
+]
